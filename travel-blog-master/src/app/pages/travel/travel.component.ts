7319eb372fa3f7f6c9b52a99f64ba19c2cf11c7e
Eman Tag <rremantag3333@gmail.com>
2025-03-05 14:30:11 +0200

    Contact Page

---
+++ travel-blog-master/src/app/pages/travel/travel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-travel',
+  standalone: false,
+  templateUrl: './travel.component.html',
+  styleUrl: './travel.component.css'
+})
+export class TravelComponent {
+
+}
