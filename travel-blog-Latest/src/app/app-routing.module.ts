7b1be44e0e58075710b786af870ede5817254d52
Tasnem Osama <temoabosamra@gmail.com>
2025-03-15 08:41:11 +0200

    Upload project

--- travel-blog-Latest/src/app/app-routing.module.ts
+++ travel-blog-Latest/src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './pages/contact/contact.component';
+
 import { HomeComponent } from './pages/home/home.component';
+import { PhotographyComponent } from './pages/photography/photography.component';
 import { SportsComponent } from './pages/sports/sports.component';
+import { TravelComponent } from './pages/travel/travel.component';
 import { BusinessComponent } from './pages/business/business.component';
+import { FashionComponent } from './pages/fashion/fashion.component';
+import { FeaturesComponent } from './pages/features/features.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'photography', component: PhotographyComponent },
+  { path: 'sports', component: SportsComponent },
+  { path: 'travel', component: TravelComponent },
+  { path: 'business', component: BusinessComponent },
+  { path: 'fashion', component: FashionComponent },
+  { path: 'features', component: FeaturesComponent },
   { path: 'contact', component: ContactComponent },
-  {path:'sports', component:SportsComponent},
-  {path:'business', component:BusinessComponent},
-  {path:'home', component:HomeComponent}
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
